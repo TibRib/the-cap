a850a4ca6ce61735fb0f8e6b49cc6e5b4111c093
TibRib <meta-block@hotmail.fr>
2020-12-15 11:44:20 +0100

    Added Angular Material

--- WebUI/the-cap-web/src/app/app.module.ts
+++ WebUI/the-cap-web/src/app/app.module.ts
@@ -6,8 +6,11 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { VideoPlayerComponent } from './video-player/video-player.component';
 import { UploadboxComponent } from './uploadbox/uploadbox.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
+/* Material Angular imports */
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -19,7 +22,8 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MDBBootstrapModule.forRoot(),
+    BrowserAnimationsModule,
+    MatSliderModule,MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
