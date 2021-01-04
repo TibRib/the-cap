40aeca54f7318710aaf4d43028b95f4555d7d5fc
TibRib <meta-block@hotmail.fr>
2021-01-04 09:34:34 +0100

    Basic interface

--- WebUI/the-cap-web/src/app/uploadbox/uploadbox.component.ts
+++ WebUI/the-cap-web/src/app/uploadbox/uploadbox.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./uploadbox.component.css']
 })
 export class UploadboxComponent implements OnInit {
+  fileToUpload: File = null;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  handleFileInput(files: FileList) {
+    this.fileToUpload = files.item(0);
+  }
+
+  uploadFileToActivity() {
+    this.fileUploadService.postFile(this.fileToUpload).subscribe(data => {
+      // do something, if upload success
+      }, error => {
+        console.log(error);
+      });
   }
 
 }
