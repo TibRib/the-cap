5c99e6c6dc9771e3a5db549bd7561756a20a8a70
gbernard314 <guillaume.bernard@isen.yncrea.fr>
2021-01-04 13:25:49 +0100

    calculating elo done

--- ranking/Athlete.py
+++ ranking/Athlete.py
@@ -1,13 +1,30 @@
 import json
 
 class Athlete:
-    def __init__(self, name='athlete_name', sport='athlete_sport', birth_year=1970, rank=1500, win=10, loss=20):
+    def __init__(self, name='athlete_name', sport='athlete_sport', birth_year=1970, rank=1500, win=[1000], loss=[2000], draw=[1500]):
+        '''
+        Parameters:
+            - win, loss and draw are juste the ranking of opponent.
+        '''
         self.name = name
         self.sport = sport
         self.birth_year = birth_year
-        self.rank = rank
         self.win = win
         self.loss = loss
+        self.draw = draw
+        self.rank = self.calculateRank()
+
+    def loose(self, opponent):
+        '''
+        When a player looses the ranking falls.
+
+        Parameters:
+
+            - self (Athlete): the looser.
+            - opponent (Athlete): the winner of the match.
+        '''
+
+
 
     def exportJSON(self):
         return '{"athlete": { \
@@ -16,5 +33,19 @@ class Athlete:
                     "birth_year": ' + str(self.birth_year) + ', \
                     "rank": ' + str(self.rank) + ', \
                     "win": ' + str(self.win) + ', \
-                    "loss": ' + str(self.loss) + ' } \
+                    "loss": ' + str(self.loss) + ', \
+                    "draw": ' + str(self.draw) + ' } \
                 }'
+
+    def calculateRank(self):
+        '''
+        ELO ranking using the rule of 400.
+        '''
+        sum_win = sum(self.win)
+        sum_loss = sum(self.loss)
+        sum_draw = sum(self.draw)
+        
+        rank_win = sum_win + 400 * len(self.win)        
+        rank_loss = sum_loss + 400 * len(self.loss)        
+        rank_draw = sum_draw + 400 * len(self.draw)
+        return int((rank_win + rank_draw + rank_loss) / (len(self.win) + len(self.draw) + len(self.loss)))
--- ranking/test_athlete.py
+++ ranking/test_athlete.py
@@ -9,18 +9,20 @@ class TestCreation(unittest.TestCase):
         self.assertEqual(ath1.name, 'athlete_name', "Wrong name")
         self.assertEqual(ath1.sport, 'athlete_sport', "Wrong sport")
         self.assertEqual(ath1.birth_year, 1970, "Wrong birth year")
-        self.assertEqual(ath1.rank, 1500, "Wrong rank")
-        self.assertEqual(ath1.win, 10, "Wrong win")
-        self.assertEqual(ath1.loss, 20, "Wrong loss")
+        self.assertEqual(ath1.rank, 1900, "Wrong rank")
+        self.assertEqual(ath1.win, [1000], "Wrong win")
+        self.assertEqual(ath1.loss, [2000], "Wrong loss")
+        self.assertEqual(ath1.draw, [1500], "Wrong draw")
 
     def test_creation_full(self):
-        ath1 = Athlete(name='john', birth_year=1999, sport='Curling', rank=1907, win=200, loss=1)
+        ath1 = Athlete(name='john', birth_year=1999, sport='Curling', rank=1907, win=[2000, 3000], loss=[1000], draw=[1200])
         self.assertEqual(ath1.name, 'john', "Wrong name")
         self.assertEqual(ath1.sport, 'Curling', "Wrong sport")
         self.assertEqual(ath1.birth_year, 1999, "Wrong birth year")
-        self.assertEqual(ath1.rank, 1907, "Wrong rank")
-        self.assertEqual(ath1.win, 200, "Wrong win")
-        self.assertEqual(ath1.loss, 1, "Wrong loss")
+        self.assertEqual(ath1.rank, 2200, "Wrong rank")
+        self.assertEqual(ath1.win, [2000, 3000], "Wrong win")
+        self.assertEqual(ath1.loss, [1000], "Wrong loss")
+        self.assertEqual(ath1.draw, [1200], "Wrong draw")
 
 class TestJSON(unittest.TestCase):
 
@@ -30,19 +32,21 @@ class TestJSON(unittest.TestCase):
         self.assertEqual(created['athlete']['name'], 'athlete_name', "Wrong name")
         self.assertEqual(created['athlete']['sport'], 'athlete_sport', "Wrong sport")
         self.assertEqual(created['athlete']['birth_year'], 1970, "Wrong birth year")
-        self.assertEqual(created['athlete']['rank'], 1500, "Wrong rank")
-        self.assertEqual(created['athlete']['win'], 10, "Wrong win")
-        self.assertEqual(created['athlete']['loss'], 20, "Wrong loss")
+        self.assertEqual(created['athlete']['rank'], 1900, "Wrong rank")
+        self.assertEqual(created['athlete']['win'], [1000], "Wrong win")
+        self.assertEqual(created['athlete']['loss'], [2000], "Wrong loss")
+        self.assertEqual(created['athlete']['draw'], [1500], "Wrong draw")
 
     def test_export_full(self):
-        ath1 = Athlete(name='john', birth_year=1999, sport='Curling', rank=1907, win=200, loss=1)
+        ath1 = Athlete(name='john', birth_year=1999, sport='Curling', rank=1907, win=[1600, 3000], loss=[], draw=[1000, 1000])
         created = json.loads(ath1.exportJSON())
         self.assertEqual(created['athlete']['name'], 'john', "Wrong name")
         self.assertEqual(created['athlete']['sport'], 'Curling', "Wrong sport")
         self.assertEqual(created['athlete']['birth_year'], 1999, "Wrong birth year")
-        self.assertEqual(created['athlete']['rank'], 1907, "Wrong rank")
-        self.assertEqual(created['athlete']['win'], 200, "Wrong win")
-        self.assertEqual(created['athlete']['loss'], 1, "Wrong loss")
+        self.assertEqual(created['athlete']['rank'], 2050, "Wrong rank")
+        self.assertEqual(created['athlete']['win'], [1600, 3000], "Wrong win")
+        self.assertEqual(created['athlete']['loss'], [], "Wrong loss")
+        self.assertEqual(created['athlete']['draw'], [1000, 1000], "Wrong draw")
 
 if __name__ == '__main__':
     unittest.main()
