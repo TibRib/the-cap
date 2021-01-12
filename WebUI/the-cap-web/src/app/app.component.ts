647f8968ec9ae3ca09e175807d9761ae20413354
Unknown <domco@mail.muni.cz>
2021-01-12 02:02:46 +0100

    WIP landing page

--- WebUI/the-cap-web/src/app/app.component.ts
+++ WebUI/the-cap-web/src/app/app.component.ts
@@ -4,12 +4,27 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <!-- barre de navigation -->
-  <nav id="navbar" class="navbar navbar-expand navbar-dark bg-dark">
-      <div class="navbar-nav">
-          <a class="nav-item nav-link" routerLink="/">Home</a>
-          <a class="nav-item nav-link" routerLink="/upload">Upload</a>
-      </div>
-  </nav>
+  <mdb-navbar SideClass="navbar navbar-expand-lg navbar-dark bg-dark">
+    <mdb-navbar-brand>
+    <img height="40" src="assets/img/THE_CAP_icon_round_flat.png">
+    </mdb-navbar-brand>
+    <links>
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-item nav-link" routerLink="/" mdbWavesEffect>Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-item nav-link" routerLink="/upload" mdbWavesEffect>Upload <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-item nav-link"  mdbWavesEffect>Pricing <span class="sr-only">(current)</span></a>
+            </li>
+            <!-- <li class="nav-item">
+                <a class="nav-link disabled" >Disabled</a>
+            </li> -->
+        </ul>
+    </links>
+</mdb-navbar>
   <!-- contenu dans un container -->
   <div class="jumbotron min-vh-100">
       <div class="container">
