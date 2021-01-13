8665f4f8c9f420c40ee155ef92b95d430da5d255
gbernard314 <guillaume.bernard@isen.yncrea.fr>
2021-01-13 22:17:00 +0100

    everything seems fine

---
+++ api.py
@@ -0,0 +1,73 @@
+import json
+from sql.sql import JSONify, getAllMatchesFromAthlete, getAllMatchesFromStartDate
+import flask
+import os 
+
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Getting data from SQLite</h1>
+<p>A prototype API for distant reading of science fiction novels.</p>'''
+
+
+# http://localhost:5000/api/info/match/name/?name=novak_djokovic&limit=10
+@app.route("/api/info/match/name/",  methods=['GET'])
+def matches_name():
+    ath_name = request.args.get("name")
+    limit = request.args.get("limit")
+    json_list = []
+    if limit == None:
+        rows = getAllMatchesFromAthlete(str(ath_name))
+    else:
+        rows = getAllMatchesFromAthlete(str(ath_name), limit=limit)
+
+    for row in rows:
+        json_list.append(JSONify(row))
+
+    return jsonify(json_list)
+
+# http://localhost:5000/api/info/match/date/?year=2020&month=07&day=01&limit=10
+@app.route("/api/info/match/date/",  methods=['GET'])
+def matches_date():
+    year = None if request.args.get("year") == None else int(request.args.get("year"))
+    month = None if request.args.get("month") == None else int(request.args.get("month"))
+    day = None if request.args.get("day") == None else int(request.args.get("day"))
+    limit = request.args.get("limit")
+
+
+    json_list = []
+    #if we have everything
+    if year != None and month != None and day != None:
+        rows = getAllMatchesFromStartDate(year=year, month=month, day=day, limit=limit)
+    #if we miss only year
+    elif year == None and month != None and day != None: 
+        rows = getAllMatchesFromStartDate(month=month, day=day, limit=limit)
+    #if we have only year
+    elif year != None and month == None and day == None: 
+        rows = getAllMatchesFromStartDate(year=year, limit=limit)
+    #if we miss only month
+    elif year != None and month == None and day != None: 
+        rows = getAllMatchesFromStartDate(year=year, day=day, limit=limit)
+    #if we have only month
+    elif year == None and month != None and day == None: 
+        rows = getAllMatchesFromStartDate(month=month, limit=limit)
+    #if we miss only day
+    elif year != None and month != None and day == None: 
+        rows = getAllMatchesFromStartDate(year=year, month=month, limit=limit)
+    #if we have only day
+    elif year == None and month == None and day != None: 
+        rows = getAllMatchesFromStartDate(day=day, limit=limit)
+    else:
+        rows = getAllMatchesFromStartDate(limit=limit)
+
+    for row in rows:
+        json_list.append(JSONify(row))
+
+    return jsonify(json_list)
+
+app.run()#host="0.0.0.0", port=80)
--- sql/sql.py
+++ sql/sql.py
@@ -1,7 +1,8 @@
 import sqlite3
+import json
 
 
-def JSONify(row):    
+def JSONify(row):
     """
     JSONify the data fetched from SQLite.
 
@@ -13,62 +14,63 @@ def JSONify(row):
 
         JSON -- The JSON object of the data.
     """
-    obj_json = {"id": row[0],
-                "start_date": row[1],
-                "end_date": row[2],
-                "location": row[3],
-                "court_surface": row[4],
-                "prize_money": row[5],
-                "currency": row[6],
-                "year": row[7],
-                "player_id": row[8],
-                "player_name": row[9],
-                "opponent_id": row[10],
-                "opponent_name": row[11],
-                "tournament": row[12],
-                "round": row[13],
-                "num_sets": row[14],
-                "sets_won": row[15],
-                "games_won": row[16],
-                "games_against": row[17],
-                "tiebreaks_won": row[18],
-                "tiebreaks_total": row[19],
-                "serve_rating": row[20],
-                "aces": row[21],
-                "double_faults": row[22],
-                "first_serve_made": row[23],
-                "first_serve_attempted": row[24],
-                "first_serve_points_made": row[25],
-                "first_serve_points_attempted": row[26],
-                "second_serve_points_made": row[27],
-                "second_serve_points_attempted": row[28],
-                "break_points_saved": row[29],
-                "break_points_against": row[30],
-                "service_games_won": row[31],
-                "return_rating": row[32],
-                "first_serve_return_points_made": row[33],
-                "first_serve_return_points_attempted": row[34],
-                "second_serve_return_points_made": row[35],
-                "second_serve_return_points_attempted": row[36],
-                "break_points_made": row[37],
-                "break_points_attempted": row[38],
-                "return_games_played": row[39],
-                "service_points_won": row[40],
-                "service_points_attempted": row[41],
-                "return_points_won": row[42],
-                "return_points_attempted": row[43],
-                "total_points_won": row[44],
-                "total_points": row[45],
-                "duration": row[46],
-                "player_victory": row[47],
-                "retirement": row[48],
-                "seed": row[49],
-                "won_first_set": row[50],
-                "doubles": row[51],
-                "masters": row[52],
-                "round_num": row[53],
-                "nation": row[54]
-                }
+    obj_json = {
+        "id": row[0],
+        "start_date": row[1],
+        "end_date": row[2],
+        "location": row[3],
+        "court_surface": row[4],
+        "prize_money": row[5],
+        "currency": row[6],
+        "year": row[7],
+        "player_id": row[8],
+        "player_name": row[9],
+        "opponent_id": row[10],
+        "opponent_name": row[11],
+        "tournament": row[12],
+        "round": row[13],
+        "num_sets": row[14],
+        "sets_won": row[15],
+        "games_won": row[16],
+        "games_against": row[17],
+        "tiebreaks_won": row[18],
+        "tiebreaks_total": row[19],
+        "serve_rating": row[20],
+        "aces": row[21],
+        "double_faults": row[22],
+        "first_serve_made": row[23],
+        "first_serve_attempted": row[24],
+        "first_serve_points_made": row[25],
+        "first_serve_points_attempted": row[26],
+        "second_serve_points_made": row[27],
+        "second_serve_points_attempted": row[28],
+        "break_points_saved": row[29],
+        "break_points_against": row[30],
+        "service_games_won": row[31],
+        "return_rating": row[32],
+        "first_serve_return_points_made": row[33],
+        "first_serve_return_points_attempted": row[34],
+        "second_serve_return_points_made": row[35],
+        "second_serve_return_points_attempted": row[36],
+        "break_points_made": row[37],
+        "break_points_attempted": row[38],
+        "return_games_played": row[39],
+        "service_points_won": row[40],
+        "service_points_attempted": row[41],
+        "return_points_won": row[42],
+        "return_points_attempted": row[43],
+        "total_points_won": row[44],
+        "total_points": row[45],
+        "duration": row[46],
+        "player_victory": row[47],
+        "retirement": row[48],
+        "seed": row[49],
+        "won_first_set": row[50],
+        "doubles": row[51],
+        "masters": row[52],
+        "round_num": row[53],
+        "nation": row[54]
+    }
 
     return obj_json
 
@@ -87,18 +89,16 @@ def getAllMatchesFromAthlete(athlete_name, limit=10000000):
     """
     conn = sqlite3.connect('sql/tennisReal.db')
     curs = conn.cursor()
-
-    curs.execute(
-        "SELECT * FROM matches WHERE player_id LIKE '%" + athlete_name + "%' LIMIT " + str(limit) + ";")
+    query = "SELECT * FROM matches WHERE player_id LIKE '%" + \
+        str(athlete_name) + "%' LIMIT " + str(limit) + ";"
+    curs.execute(query)
     rows = curs.fetchall()
 
     conn.commit()
     conn.close()
-
     return rows
 
-
-def getAllMatchesFromStartDate(year='2000', month='1', day='1', limit=10000000):
+def getAllMatchesFromStartDate(year='0', month=0, day=0, limit=10000000):
     """Get all matches for a given start_date.
 
     Keyword Arguments:
@@ -112,12 +112,37 @@ def getAllMatchesFromStartDate(year='2000', month='1', day='1', limit=10000000):
     """
     date = str(year) + '-' + "{:02d}".format(month) + \
         '-' + "{:02d}".format(day)
-    print(date)
     conn = sqlite3.connect('sql/tennisReal.db')
     curs = conn.cursor()
 
-    curs.execute(
-        "SELECT * FROM matches WHERE start_date IS '" + date + "' LIMIT " + str(limit) + ";")
+
+    if limit == None:
+        limit = 1000000
+
+    if year == '0' and month == 0 and day == 0:
+        query = ""
+    #if we have only a year
+    elif year != '0' and month == 0 and day == 0:
+        query = "SELECT * FROM matches WHERE start_date LIKE '" + str(year) + "%' LIMIT " + str(limit) + ";"
+    #if we have only a month
+    elif year == '0' and month != 0 and day == 0:
+        query = "SELECT * FROM matches WHERE start_date LIKE '%" + "{:02d}".format(month) + "%' LIMIT " + str(limit) + ";"
+    #if we have only a day
+    elif year == '0' and month == 0 and day != 0:
+        query = "SELECT * FROM matches WHERE start_date LIKE '%" + "{:02d}".format(day) + "' LIMIT " + str(limit) + ";"
+    #if we miss only the year
+    elif year == '0' and month != 0 and day != 0:
+        query = "SELECT * FROM matches WHERE start_date LIKE '%" + "{:02d}-{:02d}".format(month, day) + "' LIMIT " + str(limit) + ";"
+    #if we miss only the month
+    elif year != '0' and month == 0 and day != 0:
+        query = "SELECT * FROM matches WHERE start_date LIKE '%" + str(year) + "-%-{:02d}".format(day) + "' LIMIT " + str(limit) + ";"
+    #if we miss only the day
+    elif year != '0' and month != 0 and day == 0:
+        query = "SELECT * FROM matches WHERE start_date LIKE '%" + str(year) + "-{:02d}".format(month) + "-%' LIMIT " + str(limit) + ";"
+    else:
+        query = "SELECT * FROM matches WHERE start_date LIKE '" + str(date) + "' LIMIT " + str(limit) + ";"
+
+    curs.execute(query)
     rows = curs.fetchall()
 
     conn.commit()
@@ -125,7 +150,5 @@ def getAllMatchesFromStartDate(year='2000', month='1', day='1', limit=10000000):
 
     return rows
 
-
-all_athletes = (getAllMatchesFromAthlete('novak_djokovic', limit=2))
-print(JSONify(all_athletes[0]))
-(getAllMatchesFromStartDate(2013, 10, 7))
+#rows = getAllMatchesFromAthlete(str('novac'), limit=10)
+# print(JSONify(rows[0]))
