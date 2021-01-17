066c6fcf0d22f4b08415870fce97922b7f360c4b
TibRib <meta-block@hotmail.fr>
2021-01-17 01:40:35 +0100

    Timed AutoQueries

--- WebUI/the-cap-web/src/app/live-output-page/live-output-page.component.ts
+++ WebUI/the-cap-web/src/app/live-output-page/live-output-page.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { VisualInterpreterQueryService } from '../live-data-services/visual-interpreter-query.service';
+import { VideoPlayerComponent } from '../video-player/video-player.component';
+
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-live-output-page',
@@ -8,6 +11,8 @@ import { VisualInterpreterQueryService } from '../live-data-services/visual-inte
 })
 export class LiveOutputPageComponent implements OnInit {
   videoUrl : string = "https://storage.googleapis.com/the-cap-bucket/tennis1.mp4";
+
+  @ViewChild('videoPlayer') videoPlayer: VideoPlayerComponent;
 
   botMessages = [];
 
@@ -25,9 +30,21 @@ export class LiveOutputPageComponent implements OnInit {
       });
   }
 
+  requestAnswers() : void{
+    //Query messages every 2 seconds
+    const t1 = timer(0, 1000);
+    t1.subscribe(x => this.addMessage());
+  }
+
   //Sends to the visualAPI the url of the video
   sendVIPost(): void{
     this.vi_api.postURL(this.videoUrl)
+
+    //Data is sent, let's listen for responses...
+    const t2 = timer(5000);
+    this.requestAnswers()
+    t2.subscribe(x => this.videoPlayer.play());
+
   }
 
 }
