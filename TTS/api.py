e5b6580a65f38d269334f9798b3420d5e52020b4
= DPierre <p.desoomer@gmail.com>
2021-01-20 16:42:17 +0100

    Added TTS folder

---
+++ TTS/api.py
@@ -0,0 +1,36 @@
+import flask
+from flask import redirect, request, jsonify, render_template
+import os
+import base64
+
+app = flask.Flask(__name__, template_folder='template')
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Distant Reading Archive</h1>
+    <p>A prototype API for distant reading of science fiction novels.</p>'''
+
+
+@app.route('/post/<filename>', methods=['GET', 'POST'])
+def result(filename):
+    file = base64.b64decode(filename).decode('utf-8')
+    os.system("./bin/flite '" + file + "' audio.wav")
+    if os.path.exists('audio.wav'):
+        enc = base64.b64encode(open("audio.wav", 'rb').read())
+        return enc
+    else:
+        return "Oops, looks like the file has not been created, try again !"
+
+
+@app.route('/get/', methods=['GET'])
+def returnFile():
+    if os.path.exists('audio.wav'):
+        enc = base64.b64encode(open("audio.wav", 'rb').read())
+        return enc
+    else:
+        return "Oops, looks like the file has not been created, try again !"
+
+
+app.run(host='0.0.0.0', port=8181)
